b473240142bcac049c752f47d72aa3d0f71389c5
zam-dev <zamsyh.work@gmail.com>
2022-04-03 15:28:32 +0700

    Setting up firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: "evernote-f9946.firebaseapp.com",
+    projectId: "evernote-f9946",
+    storageBucket: "evernote-f9946.appspot.com",
+    messagingSenderId: "105930331629",
+    appId: "1:105930331629:web:0959a6c5bcd425357cc3e4"
+  
+  };
+
+const app = initializeApp(firebaseConfig);
